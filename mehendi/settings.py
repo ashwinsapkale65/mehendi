408f4694255f566173b83b68ce2217e1c0f263cc
ashwin <ashwinsapkale65@gmail.com>
2022-08-14 20:59:22 +0530

    newpush

--- mehendi/settings.py
+++ mehendi/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #managing media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'

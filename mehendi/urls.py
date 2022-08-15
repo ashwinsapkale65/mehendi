501b778a4b7cda8693186435bba357c0480b3c0a
ashwin <ashwinsapkale65@gmail.com>
2022-08-15 14:15:27 +0530

    newone

--- mehendi/urls.py
+++ mehendi/urls.py
@@ -19,6 +19,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+from django.conf import settings
+
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mehendihome.urls'))

635b8aab6d2136286eefd328aeee8a2288a6f448
ashwinsapkale65 <107023935+ashwinsapkale65@users.noreply.github.com>
2022-08-15 14:02:23 +0530

    Update settings.py

--- mehendi/settings.py
+++ mehendi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zug0v_9iuueges9o6&i&z5pc^!o$3tw7!)v7=y4skboub(hf@w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['divyaart.herokuapp.com']
 
